bcff9d8ab5ed3bda7620b367fc3e9ead88dfee2a
clapsheep <sktndid1203@naver.com>
2023-12-17 03:31:58 +0900

    mission2완성

--- mission01/naver_login/js/yamoo.js
+++ mission01/naver_login/js/yamoo.js
@@ -14,7 +14,7 @@ console.log(pass);
 
 
 */
-const 합격_기준_점수 = 70;
+/*const 합격_기준_점수 = 70;
 let 응시자_수 = prompt("시험에 응시한 학생 수는 몇 명인가요?");
 let 합격자_수 = 0;
 응시자_수 = Number(응시자_수); // 문자 -> 숫자형으로 변형
@@ -35,7 +35,7 @@ console.log(
     (합격자_수 / 응시자_수) * 100
   }%)`
 );
-
+*/
 /*{
   // 시작
   const 합격_기준_점수 = 70;
@@ -64,3 +64,15 @@ console.log(
   // 복합 구조
   // 끝
 }*/
+
+function setRandomInt() {
+  return Math.floor(Math.random() * 10);
+}
+const 밑변 = setRandomInt() + 1;
+console.log(`밑변 : ${밑변}`);
+const 높이 = setRandomInt() + 1;
+console.log(`높이: ${높이}`);
+
+const 넓이 = (밑변, 높이) => 밑변 * 높이 * 0.5;
+삼각형 = 넓이(밑변, 높이);
+console.log(`넓이: ${삼각형}`);
